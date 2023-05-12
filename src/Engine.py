eec4d48e915bb304a19565fb3aa9887cad91a1f5
EightOhFour <73658510+Milkman337@users.noreply.github.com>
2023-05-12 14:21:15 +0200

    fixed a little bug

--- src/Engine.py
+++ src/Engine.py
@@ -1,15 +1,15 @@
 from pyray import *
 from typing import Callable
 
-def run_engine(cam:Camera2D, update: callable[[], None], render: callable[[], None], render_without_cam:callable[[], None]=None):
+def run_engine(cam:Camera2D, update, render, render_without_cam=None):
     """
     Starts the GameEngine.
 
     Args:
         cam (Camera2D): The camera you want to render with.\n
-        update (callable[[], None]): gets called every frame and is used for logic.\n
-        render (callable[[], None]): gets called once every frame and is used for rendering.\n
-        render_without_cam (callable[[], None]): same as render but is not rendered with the camera. This can be used for GUI.\n
+        update (function): gets called every frame and is used for logic.\n
+        render (function): gets called once every frame and is used for rendering.\n
+        render_without_cam (function): same as render but is not rendered with the camera. This can be used for GUI.\n
     
     Return:
         None
